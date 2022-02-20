7eeed77a9d1af3ad3f24c807cf05a48430cb26cd
Lenard <bebobhd@gmail.com>
2022-02-20 15:39:00 +0000

    bot works

--- venv/main.py
+++ venv/main.py
@@ -1,35 +1,43 @@
 import discord
 from discord.ext import commands
 import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = logging.getLogger()
 
-token = "OTQzMTY4NDY2OTYwMTUwNTY5.YgvIFQ.qzxs7tRn4Jq6UwgXrVlEMOkBx3Q"
+token = os.getenv('TOKEN')
 client = discord.Client()
 
 def find_channel():
   text_channel_list = []
-  for server in discord.Client.servers:
-    for channel in server.channels:
-      if channel.type == 'Text':
-        text_channel_list.append(channel)
+  for guild in client.guilds:
+    for channel in guild.text_channels:
+      text_channel_list.append(channel)
   return text_channel_list
 
 @client.event
 async def on_ready():
-  logger.debug("starting as {0.user}".format(client))
-  id = find_channel()[0]
-  channel = client.get_channel(id)
-  await channel.send('Hello :wave:')
+  print("starting as {0.user}".format(client))
+  channels = find_channel()
+  await channels[0].send('dramatic entry :boom:')
 
 
 @client.event
 async def on_message(message):
+  channels = find_channel()
   if message.author == client.user:
     return
   if message.content.startswith("%hello"):
     await message.channel.send("HI!")
   elif message.content.startswith("%test"):
     await message.channel.send(":pensive:")
+  elif message.content.startswith("%doit"):
+    await message.channel.send("say less :muscle:")
+    await channels[1].send("*neko")
+
+
 
 client.run(token)
